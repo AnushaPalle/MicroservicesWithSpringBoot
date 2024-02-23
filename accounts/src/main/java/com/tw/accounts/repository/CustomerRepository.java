ffc3d584a3b055b3b864e228d6c2758a955a9af7
AnushaPalle <anushapalle11@gmail.com>
2024-02-23 14:00:35 +0530

    Implement Accounts Microservice

---
+++ accounts/src/main/java/com/tw/accounts/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.tw.accounts.repository;
+
+import com.tw.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
